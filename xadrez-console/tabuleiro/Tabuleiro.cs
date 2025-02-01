36a06145d8fc0c3663a4dadaa7118279aa3485c5
Marcelo Magalhães <celoxd01@gmail.com>
2025-02-01 16:41:41 -0300

    Começando a colocar peças no tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,6 +1,6 @@
 ﻿
 
-using xadrez_console.tabuleiro;
+using tabuleiro;
 
 namespace tabuleiro
 {
@@ -20,6 +20,12 @@ namespace tabuleiro
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos) // colocar Peca p na posição Pos
+        {
+            pecas[pos.linha, pos.coluna] = p; // é ir lá na matriz de pecas e jogar a peca p
+            p.posicao = pos; // ir na p e dizer q a posição da p agr é pós
         }
     }
 }
