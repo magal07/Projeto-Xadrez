a3a68ac843e3817b843ae780aee9500949900a82
Marcelo Magalhães <celoxd01@gmail.com>
2025-02-01 17:14:49 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,26 +8,24 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            /* try
-             {
-                 Tabuleiro tab = new Tabuleiro(8, 8);
 
-                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-                 Tela.imprimirTabuleiro(tab);
-             }
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
+                Tela.imprimirTabuleiro(tab);
+            }
              catch(TabuleiroException e)
              {
-                 Console.WriteLine(e.Message);
-             }
-             Console.WriteLine(); */
-
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
-            Console.WriteLine();
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine(); 
         }
     }
 }
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -7,6 +7,7 @@ namespace xadrez_console
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
             for (int i = 0; i < tab.linhas; i++) {
+                Console.Write(8 -i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if(tab.peca(i, j) == null)
@@ -15,10 +16,27 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " "); // tab.peca = chamando peça no argumento tab
+                        imprimirPeca(tab.peca(i, j));
+                        //Console.Write(tab.peca(i, j) + " "); // tab.peca = chamando peça no argumento tab // version initial
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if(peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor; // CRIANDO A VARIÁVEL E BUSCANDO COR PRIMÁRIA DO CONSOLE (CINZA)
+                Console.ForegroundColor = ConsoleColor.Yellow; // ALTERANDO A COR DA VARIÁVEL aux PARA YELLOW
+                Console.Write(peca); // chamando peça
+                Console.ForegroundColor = aux;
             }
         }
     }
